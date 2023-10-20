dea6f37ee451770a7b9a8730509b66f5d0a5b000
Hong Wu <hong.wu@rea-group.com>
2023-10-20 14:54:55 +0800

    refactor: remove useless comments

--- auto/connect.sh
+++ auto/connect.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Warning: the "mongo" shell has been superseded by "mongosh", which delivers improved usability and compatibility.The "mongo" shell has been deprecated and will be removed in an upcoming release. For installation instructions, see https://docs.mongodb.com/mongodb-shell/install/
-# docker exec -it mongodb mongo admin -u root -p 'example'
-
-
 # https://docs.mongodb.com/mongodb-shell/
 # https://www.mongodb.com/docs/mongodb-shell/
 # https://www.mongodb.com/docs/mongodb-shell/connect/
